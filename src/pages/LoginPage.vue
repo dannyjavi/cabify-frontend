de8f33ec9f1afec66273b5fc9df7569851303cfd
juan <jahsxp@hotmail.com>
2020-10-23 14:15:34 +0200

    login page añadido

---
+++ src/pages/LoginPage.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="modal is-active">
+  <div class="modal-background"></div>
+  <div class="modal-card">
+    <header class="modal-card-head">
+      <p class="modal-card-title">Account Login</p>
+      <button class="delete" aria-label="close"></button>
+    </header>
+    <section class="modal-card-body">
+      <label class="label">User Email</label>
+      <input class="input" type="email">
+      <label class="label">User Password</label>
+      <input class="input" type="text">
+    </section>
+    <footer class="modal-card-foot">
+      <button class="button is-success">Log In</button>
+      <button class="button is-info">Remember Me</button>
+    </footer>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import HomePage from "../pages/HomePage";
+import LoginPage from "../pages/LoginPage";
 
 Vue.use(VueRouter);
 
@@ -9,6 +10,11 @@ const routes = [
     path: "/",
     name: "Home",
     component: HomePage
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: LoginPage
   }
 ];
 
