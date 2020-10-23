acf6158cf903b66cbe2ac29c7ce726b5cab59228
juan <jahsxp@hotmail.com>
2020-10-23 10:04:59 +0200

    vue creado

---
+++ cabify-app/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <MainNav />
+    <router-view />
+    <MainFooter />
+  </div>
+</template>
+
+<script>
+import MainNav from './components/MainNav'
+import MainFooter from './components/MainFooter'
+export default {
+  name: 'App',
+  components: { MainNav, MainFooter }
+}
+</script>
+
+<style>
+
+</style>
