23a7aaa7ec900a2acaa45561334b379de3ce3253
juan <jahsxp@hotmail.com>
2020-10-26 15:41:41 +0100

    login style

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -8,7 +8,9 @@
 export default {
   name: 'Map',
   mounted() {
-    var mymap = L.map('mapid').setView([36.72, -4.42], 15);
+    let mymap = L.map('mapid').setView([36.72, -4.42], 15);
+    mymap.locate({ enableHighAccuracy: true })
+    mymap.locate({ setView: true })
     let tileURL = 'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw'
   	L.tileLayer( tileURL, {
     minZoom: 14,
@@ -17,8 +19,11 @@ export default {
 		tileSize: 512,
 		zoomOffset: -1
   }).addTo(mymap);
-  var marker = L.marker([36.72, -4.42]).bindPopup('You are here!').addTo(mymap)
-  mymap.L.locate({enableHighAccuracy: true})
+  mymap.on('locationfound', e => {
+    const coords = [e.latlng.lat, e.latlng.lng]
+    let marker = L.marker(coords).bindPopup('You are here!').addTo(mymap)
+    console.log(e)
+  })
   }
 }
 </script>
--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,47 +1,42 @@
 <template>
   <div>
-    <div class="modal is-active">
-      <div class="modal-background"></div>
-      <div class="modal-card">
-        <header class="modal-card-head">
-          <p class="modal-card-title">Account Login</p>
-          <button class="delete" aria-label="close"></button>
-        </header>
-        <section class="modal-card-body">
-          <div class="field">
-            <p class="control has-icons-left has-icons-right">
-              <input class="input" type="email" placeholder="Email" />
-              <span class="icon is-small is-left">
-                <i class="fas fa-envelope"></i>
-              </span>              
-            </p>
-          </div>
-          <div class="field">
-            <p class="control has-icons-left">
-              <input class="input" type="password" placeholder="Password" />
-              <span class="icon is-small is-left">
-                <i class="fas fa-lock"></i>
-              </span>
-            </p>
-          </div>
-          <div class="field">
-            <p class="control">
-              <button class="button is-dark is-focused">Login</button>              
-            </p>
-          </div>
-        </section>
-        <footer class="modal-card-foot">
-          <div class="field is-grouped">
-            <p class="control">              
-              <a>Don't have an account yet? Sign up here!</a> 
-            </p>
-            <p class="control">              
-              <a>Forgot Password?</a> 
-            </p>
-          </div>
-        </footer>
+    <header class="mt-6">
+      <p class="title is-2 has-text-centered has-text-white">Account Login</p>
+    </header>
+    <section class="card-content has-background-success">
+      <div class="field">
+        <p class="control has-icons-left has-icons-right">
+          <input class="input is-medium" type="email" placeholder="Email" />
+          <span class="icon is-small is-left">
+            <i class="fas fa-envelope"></i>
+          </span>
+        </p>
       </div>
-    </div>
+      <div class="field">
+        <p class="control has-icons-left">
+          <input class="input is-medium" type="password" placeholder="Password" />
+          <span class="icon is-small is-left">
+            <i class="fas fa-lock"></i>
+          </span>
+        </p>
+      </div>
+      <div class="field">
+        <p class="control">
+          <button class="button is-dark is-focused is-medium is-fullwidth title">Login</button>
+        </p>
+      </div>
+    </section>
+    
+    <footer class="container">
+      <div class="field">
+        <p class="control has-text-centered">
+          <a class="has-text-primary-light title is-4">Don't have an account yet? Sign up here!</a>
+        </p> <br>
+        <p class="control has-text-centered">
+          <a class="has-text-primary-light title is-4">Forgot Password?</a>
+        </p>
+      </div>
+    </footer>
   </div>
 </template>
 
@@ -50,11 +45,7 @@ export default {};
 </script>
 
 <style>
-  .modal-background {
-    background: #279D79
-  }
-
-  
-
-  
+ .container {
+   width: 80%;
+ }
 </style>
