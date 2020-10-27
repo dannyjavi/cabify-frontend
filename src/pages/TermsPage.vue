4233ff6fecc1ec50e10fe3292848db547b7b2452
RocioRlg <rociorlg73@gmail.com>
2020-10-27 10:04:49 +0100

    cambios terms y login

--- src/pages/TermsPage.vue
+++ src/pages/TermsPage.vue
@@ -1,13 +1,43 @@
 <template>
-  
+  <div class="container mt-4 has-text-centered has-text-white">
+    <header>
+      <div>
+        <p>
+          Aviso legal y condiciones generales de uso El aviso legal y las
+          condiciones generales de uso, o términos y condiciones de uso, está
+          pensado para todos aquellos que van a crear una página o sitio web.
+          Este documento está adaptado al Reglamento Europeo de Protección de
+          Datos (RGPD) y a la reciente Ley Orgánica 3/2018, de 5 de diciembre,
+          de Protección de Datos Personales y garantía de los derechos
+          digitales. Este documento contiene, de forma general, un conjunto de
+          menciones o referencias legales que servirán para regular las
+          condiciones en las que los usuarios de Internet acceden, navegan y
+          utilizan el sitio o página web. También comprende los derechos,
+          obligaciones y responsabilidades a los que las partes quedan sometidas
+          recíprocamente durante el acceso, navegación y uso del sitio web. La
+          inclusión de un aviso legal y condiciones generales de uso aporta un
+          gran nivel de confianza y seguridad jurídica a todos y cada uno de los
+          sitios web presentes en Internet. También existe otro conjunto de
+          menciones legales —condiciones generales de venta— que, a veces, se
+          presentan de forma conjunta con el aviso legal y las condiciones
+          generales de uso, pero no deben confundirse el uno con el otro, pues
+          su contenido difiere. Así, mientras el aviso legal regula las
+          condiciones en las que los usuarios de Internet acceden navegan y
+          utilizan el sitio web, las condiciones generales de venta regulan las
+          condiciones en las que se realizan los pedidos o solicitudes de compra
+          en el sitio web, y establecen los derechos, obligaciones y
+          responsabilidades que corresponden a cada parte de dicha compraventa.
+        </p>
+      </div>
+    </header>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'TermsPage'
-}
+  name: "TermsPage",
+};
 </script>
 
 <style>
-
 </style>
